6012b8f4a3e74a1a14e44d009433044baec64aa3
bluwy <bjornlu.dev@gmail.com>
2021-05-01 22:22:05 +0800

    Improve source detection

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,14 @@ export interface AssetSource {
   tag: string
   srcAttrs?: string[]
   srcsetAttrs?: string[]
+  filter?: (metadata: FilterMetadata) => boolean
+}
+
+export interface FilterMetadata {
+  tag: string
+  attribute: string
+  value: string
+  attributes: Record<string, string>
 }
 
 export default function importAssets(
@@ -41,7 +49,7 @@ export default function importAssets(
         start: number
         end: number
       }) {
-        const url = attributeValue.raw
+        const url = attributeValue.raw.trim()
 
         if (urlFilter && !urlFilter(url)) return
 
@@ -72,38 +80,70 @@ export default function importAssets(
               return
             }
 
+            let lazyAttributes: Record<string, string> | undefined
+
+            function getAttributes() {
+              if (!lazyAttributes) {
+                lazyAttributes = {}
+                node.attributes.forEach((attr: any) => {
+                  // Ensure text only, since text only attribute values will only have one element
+                  if (attr.value.length > 1 && attr.value[0].type !== 'Text')
+                    return
+                  lazyAttributes[attr.name] = attr.value[0].raw
+                })
+              }
+              return lazyAttributes
+            }
+
             for (let i = 0; i < sources.length; i++) {
               const source: AssetSource = sources[i]
 
               // Compare node tag match
               if (source.tag === node.name) {
+                function getAttrValue(attr: string) {
+                  const attribute = node.attributes.find((v) => v.name === attr)
+                  if (!attribute) return
+
+                  // Ensure text only, since text only attribute values will only have one element
+                  if (
+                    attribute.value.length > 1 &&
+                    attribute.value[0].type !== 'Text'
+                  )
+                    return
+
+                  if (
+                    source.filter &&
+                    !source.filter({
+                      tag: source.tag,
+                      attribute: attr,
+                      value: content.slice(attribute.start, attribute.end),
+                      attributes: getAttributes(),
+                    })
+                  )
+                    return
+
+                  return attribute.value[0]
+                }
+
                 // Check src
                 source.srcAttrs?.forEach((attr) => {
-                  const attribute = node.attributes.find((v) => v.name === attr)
-                  if (attribute) {
-                    const v = attribute.value[0]
-                    if (v.type === 'Text') {
-                      addImport(v)
-                    }
-                  }
+                  const value = getAttrValue(attr)
+                  if (!value) return
+                  addImport(value)
                 })
 
                 // Check srcset
                 source.srcsetAttrs?.forEach((attr) => {
-                  const attribute = node.attributes.find((v) => v.name === attr)
-                  if (attribute) {
-                    const v = attribute.value[0]
-                    if (v.type === 'Text') {
-                      const srcsetRegex = /\s*([^,]\S*).*?(?:,|$)\s*/gm
-                      let match: RegExpExecArray
-                      while ((match = srcsetRegex.exec(v.raw))) {
-                        addImport({
-                          raw: match[1],
-                          start: v.start + match.index,
-                          end: v.start + match.index + match[1].length,
-                        })
-                      }
-                    }
+                  const value = getAttrValue(attr)
+                  if (!value) return
+                  const srcsetRegex = /\s*([^,]\S*).*?(?:,|$)\s*/gm
+                  let match: RegExpExecArray
+                  while ((match = srcsetRegex.exec(value.raw))) {
+                    addImport({
+                      raw: match[1],
+                      start: value.start + match.index,
+                      end: value.start + match.index + match[1].length,
+                    })
                   }
                 })
 
@@ -139,6 +179,53 @@ export default function importAssets(
 const IGNORE_FLAG = 'svelte-preprocess-import-assets-ignore'
 
 const DEFAULT_ASSET_PREFIX = '__ASSET__'
+
+const ALLOWED_REL = [
+  'stylesheet',
+  'icon',
+  'shortcut icon',
+  'mask-icon',
+  'apple-touch-icon',
+  'apple-touch-icon-precomposed',
+  'apple-touch-startup-image',
+  'manifest',
+  'prefetch',
+  'preload',
+]
+
+const ALLOWED_ITEMPROP = [
+  'image',
+  'logo',
+  'screenshot',
+  'thumbnailurl',
+  'contenturl',
+  'downloadurl',
+  'duringmedia',
+  'embedurl',
+  'installurl',
+  'layoutimage',
+]
+
+const ALLOWED_META_NAME = [
+  'msapplication-tileimage',
+  'msapplication-square70x70logo',
+  'msapplication-square150x150logo',
+  'msapplication-wide310x150logo',
+  'msapplication-square310x310logo',
+  'msapplication-config',
+  'twitter:image',
+]
+
+const ALLOWED_META_PROPERTY = [
+  'og:image',
+  'og:image:url',
+  'og:image:secure_url',
+  'og:audio',
+  'og:audio:secure_url',
+  'og:video',
+  'og:video:secure_url',
+  'vk:image',
+]
 
 const DEFAULT_SOURCES: AssetSource[] = [
   {
@@ -187,5 +274,51 @@ const DEFAULT_SOURCES: AssetSource[] = [
     tag: 'link',
     srcAttrs: ['href'],
     srcsetAttrs: ['imagesrcset'],
+    filter({ attributes }) {
+      if (
+        attributes.rel &&
+        ALLOWED_REL.includes(attributes.rel.trim().toLowerCase())
+      ) {
+        return true
+      }
+
+      if (
+        attributes.itemprop &&
+        ALLOWED_ITEMPROP.includes(attributes.itemprop.trim().toLowerCase())
+      ) {
+        return true
+      }
+
+      return false
+    },
+  },
+  {
+    tag: 'meta',
+    srcAttrs: ['content'],
+    filter({ attributes }) {
+      console.log(attributes)
+      if (
+        attributes.name &&
+        ALLOWED_META_NAME.includes(attributes.name.trim().toLowerCase())
+      ) {
+        return true
+      }
+
+      if (
+        attributes.property &&
+        ALLOWED_META_PROPERTY.includes(attributes.property.trim().toLowerCase())
+      ) {
+        return true
+      }
+
+      if (
+        attributes.itemprop &&
+        ALLOWED_ITEMPROP.includes(attributes.itemprop.trim().toLowerCase())
+      ) {
+        return true
+      }
+
+      return false
+    },
   },
 ]
--- test/Input.svelte
+++ test/Input.svelte
@@ -10,8 +10,18 @@
 <video poster="./foo.mp4" />
 <image href="./foo.mp4" xlink:href="./foo.mp4" />
 <use href="./foo.mp4" xlink:href="./foo.mp4" />
-<link href="./foo.css" imagesrcset="./foo.css 100v, ./bar.css 200v" />
 <!-- svelte-preprocess-import-assets-ignore -->
 <use href="./foo.mp4" xlink:href="./foo.mp4" />
 <use href="./foo.mp4" xlink:href="./foo.mp4" />
 <img src="./foo.exe" />
+<link
+  rel="stylesheet"
+  href="./foo.css"
+  imagesrcset="./foo.css 100v, ./bar.css 200v"
+/>
+<link href="./foo.css" />
+<link itemprop="logo" href="./foo.jpg" />
+<meta name="author" content="Bjorn Lu" />
+<meta name="twitter:image" content="./foo.pdf" />
+<meta property="og:image" content="./foo.png" />
+<meta itemprop="embedurl" content="./foo.mp3" />
