d732d5925a7a6414685cd7e121374ee325543bd9
bluwy <bjornlu.dev@gmail.com>
2022-10-02 17:13:30 +0800

    Split out files

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface ImportAssetsOptions {
+  sources?: AssetSource[] | ((defaultSources: AssetSource[]) => AssetSource[])
+  importPrefix?: string
+  http?: boolean
+  urlFilter?: (url: string) => boolean
+}
+
+export interface AssetSource {
+  tag: string
+  srcAttributes?: string[]
+  srcsetAttributes?: string[]
+  filter?: (metadata: FilterMetadata) => boolean
+}
+
+export interface FilterMetadata {
+  tag: string
+  attribute: string
+  value: string
+  attributes: Record<string, string>
+}
